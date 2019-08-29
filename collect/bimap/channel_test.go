45cb5f7ca7838330661311fee856d3bd065685e9
Connor Newton <connor@ifthenelse.io>
2019-08-29 22:45:08 +0100

    Add missing Bimap Channel tests

--- collect/bimap/channel_test.go
+++ collect/bimap/channel_test.go
@@ -23,3 +23,40 @@ func TestChannel_Values(t *testing.T) {
 	}
 }
 
+func TestChannel_Keys(t *testing.T) {
+	for name, test := range delegateTests {
+		t.Run(name, func(t *testing.T) {
+			want := setFromSlice(test.k...)
+			m := bimapFromSlice(test.p...)
+			d := Channel{M: m}
+			got := make(set.Set, len(test.k))
+			for k := range d.Keys() {
+				got[k] = struct{}{}
+			}
+			if !reflect.DeepEqual(want, got) {
+				t.Fatalf("unexpected keys: want %v, got %v", want, got)
+			}
+		})
+	}
+}
+
+func TestChannel_Pairs(t *testing.T) {
+	for name, test := range delegateTests {
+		t.Run(name, func(t *testing.T) {
+			p := make([]interface{}, len(test.p))
+			for i := range test.p {
+				p[i] = test.p[i]
+			}
+			want := setFromSlice(p...)
+			m := bimapFromSlice(test.p...)
+			d := Channel{M: m}
+			got := make(set.Set, len(test.p))
+			for p := range d.Pairs() {
+				got[p] = struct{}{}
+			}
+			if !reflect.DeepEqual(want, got) {
+				t.Fatalf("unexpected pairs: want %v, got %v", want, got)
+			}
+		})
+	}
+}
